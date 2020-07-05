16aafce818c74489077f35a2876c16500caa1c2a
Duc Nguyen <minhduc140583@gmail.com>
2020-07-05 13:33:32 +0700

    pure function

--- src/toast.ts
+++ src/toast.ts
@@ -1,50 +1,52 @@
 export interface ToastService {
-  showToast(msg: string);
+  showToast(msg: string): void;
 }
 
-export class ToastUtil {
-  private static sysToast = null;
-  static showToast(msg: string) {
-    if (!ToastUtil.sysToast) {
-      ToastUtil.sysToast = (window as any).sysToast;
+// tslint:disable-next-line:class-name
+export class resource {
+  static toast = null;
+}
+
+export function fadeIn(el: any, display = null): void {
+  el.style.opacity = 0;
+  el.style.display = display || 'block';
+
+  (function fade() {
+    let val = parseFloat(el.style.opacity);
+    val += .1;
+    if (!(val > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
     }
-    ToastUtil.sysToast.innerHTML = msg;
-    ToastUtil.fadeIn(ToastUtil.sysToast);
-    setTimeout(() => {
-      ToastUtil.fadeOut(ToastUtil.sysToast);
-    }, 1340);
-  }
+  })();
+}
 
-  private static fadeOut(el) {
-    el.style.opacity = 1;
-    (function fade() {
-      const k = (el.style.opacity -= .1);
-      if (k < 0) {
-        el.style.display = 'none';
-      } else {
-        requestAnimationFrame(fade);
-      }
-    })();
-  }
+export function fadeOut(el: any): void {
+  el.style.opacity = 1;
+  (function fade() {
+    const k = (el.style.opacity -= .1);
+    if (k < 0) {
+      el.style.display = 'none';
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
+}
 
-  private static fadeIn(el, display = null) {
-    el.style.opacity = 0;
-    el.style.display = display || 'block';
-
-    (function fade() {
-      let val = parseFloat(el.style.opacity);
-      val += .1;
-      if (!(val > 1)) {
-        el.style.opacity = val;
-        requestAnimationFrame(fade);
-      }
-    })();
+export function showToast(msg: string): void {
+  if (!resource.toast) {
+    resource.toast = (window as any).sysToast;
   }
+  resource.toast.innerHTML = msg;
+  fadeIn(resource.toast);
+  setTimeout(() => {
+    fadeOut(resource.toast);
+  }, 1340);
 }
 
 class DefaultToastService {
-  showToast(msg: string) {
-    ToastUtil.showToast(msg);
+  showToast(msg: string): void {
+    showToast(msg);
   }
 }
 
